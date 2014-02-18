b0169ebfc5a3d8210934bb29b7c3e029cc5e3ed0
Denis Vorobyov <vorobjevda@hotmail.com>
2014-02-18 19:05:25 +0400

    Today workout;

--- gen/ru/vorobjev/rbknews/R.java
+++ gen/ru/vorobjev/rbknews/R.java
@@ -24,10 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int lister=0x7f080001;
+        public static final int action_settings=0x7f080004;
+        public static final int home_btn_messages=0x7f080002;
+        public static final int home_btn_new_message=0x7f080001;
         public static final int splashscreen=0x7f080000;
-        public static final int webview=0x7f080002;
+        public static final int webview=0x7f080003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -39,6 +40,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int btn_list_news=0x7f050003;
+        public static final int btn_preferences=0x7f050004;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
@@ -68,5 +71,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int DashboardButton=0x7f060002;
     }
 }
---
+++ src/ru/vorobjev/rbknews/RssItem.java
@@ -0,0 +1,44 @@
+package ru.vorobjev.rbknews;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class RssItem {
+
+	private String title;
+	private String description;
+	private Date pubDate;
+	private String link;
+
+	public RssItem(String title, String description, Date pubDate, String link) {
+		this.title = title;
+		this.description = description;
+		this.pubDate = pubDate;
+		this.link = link;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public String getLink() {
+		return this.link;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public Date getPubDate() {
+		return this.pubDate;
+	}
+
+	@Override
+	public String toString() {
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd - hh:mm:ss", Locale.getDefault());
+		String result = getTitle() + "  ( " + sdf.format(this.getPubDate()) + " )";
+		return result;
+	}
+
+}
